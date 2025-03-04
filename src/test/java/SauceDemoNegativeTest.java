43beeab307b5c7cc041b096787eb1ea65cc21bfd
Владислав <vmarkushin04@mail.ru>
2025-03-04 11:24:31 +0700

    Updated!

--- src/test/java/SauceDemoNegativeTest.java
+++ src/test/java/SauceDemoNegativeTest.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 public class SauceDemoNegativeTest {
+
+    @BeforeEach
+    void setUp() {
+        Configuration.baseUrl = "https://www.saucedemo.com";
+        open("/");
+        WebDriver driver = Selenide.webdriver().driver().getWebDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void loginTestWithInvalidCredentials() {
+        $("#user-name").setValue("invalid_user");
+        $("#password").setValue("invalid_password");
+        $("#login-button").click();
+
+        $("[data-test='error']").shouldBe(visible)
+                .shouldHave(text("Epic sadface: Username and password do not match any user in this service"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWebDriver();
+    }
 }
--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 public class SauceDemoTest {
+
+    @BeforeEach
+    void setUp() {
+        Configuration.baseUrl = "https://www.saucedemo.com";
+        open("/");
+        WebDriver driver = Selenide.webdriver().driver().getWebDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    void loginTest() {
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+
+        $(".inventory_list").shouldBe(visible);
+    }
+
+    @AfterEach
+    void tearDown() {
+        closeWebDriver();
+    }
 }
